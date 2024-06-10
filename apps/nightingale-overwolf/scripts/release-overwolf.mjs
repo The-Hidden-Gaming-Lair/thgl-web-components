01989b71f2ff9f977ca42c77f69a0194cdf839c2
lmachens <leon@machens.koeln>
2024-06-10 10:44:10 +0200

    Add logs

--- apps/nightingale-overwolf/scripts/release-overwolf.mjs
+++ apps/nightingale-overwolf/scripts/release-overwolf.mjs
@@ -13,9 +13,14 @@ import {
 dotenv.config({ path: ".env.local" });
 
 const PREVIEW_ACCESS_CHANNEL_ID = process.env.PREVIEW_ACCESS_CHANNEL_ID;
+
+console.log(
+  `About to release to channel ${PREVIEW_ACCESS_CHANNEL_ID} with ${process.env.OW_CLI_EMAIL}`,
+);
 const FILE_NAME = "app";
 
 OwCliContainer.init();
+console.log("Initialized OW CLI container");
 
 const packOpkCmd = OwCliContainer.resolve(PackOpkCommand);
 const signOpkCmd = OwCliContainer.resolve(SignOpkCommand);
@@ -26,28 +31,34 @@ await packOpkCmd.handler({
   folderPath: "dist",
   outputFile: `${FILE_NAME}.opk`,
 });
+console.log("Packed OPK");
 await signOpkCmd.handler({
   filePath: `${FILE_NAME}.opk`,
   outputFile: `${FILE_NAME}.signed.opk`,
 });
+console.log("Signed OPK");
 if (PREVIEW_ACCESS_CHANNEL_ID) {
   const versionId = await uploadOpkCmd.handler({
     filePath: `${FILE_NAME}.signed.opk`,
     channelId: Number(PREVIEW_ACCESS_CHANNEL_ID),
     wait: true,
   });
+  console.log("Uploaded OPK");
   await releaseOpkCmd.handler({
     versionId,
     percent: 100,
     channelId: Number(PREVIEW_ACCESS_CHANNEL_ID),
   });
+  console.log("Released OPK");
 } else {
   const versionId = await uploadOpkCmd.handler({
     filePath: `${FILE_NAME}.signed.opk`,
     wait: true,
   });
+  console.log("Uploaded OPK");
   await releaseOpkCmd.handler({
     versionId,
     percent: 100,
   });
+  console.log("Released OPK");
 }
