f2192db52ad5e1bdc1ad0e57f02c85ebf0c42244
lmachens <leon@machens.koeln>
2024-06-25 16:07:49 +0200

    Prepare HL

---
+++ apps/hogwarts-legacy-web/src/app/page.tsx
@@ -0,0 +1,69 @@
+import dynamic from "next/dynamic";
+import { MarkersSearch } from "@repo/ui/controls";
+import type {
+  FiltersCoordinates,
+  GlobalFiltersCoordinates,
+  NodesCoordinates,
+  RegionsCoordinates,
+} from "@repo/ui/providers";
+import { FloatingAds } from "@repo/ui/ads";
+import { CoordinatesProvider } from "@repo/ui/providers";
+import { HeaderOffset } from "@repo/ui/header";
+import type { Metadata } from "next";
+import { searchParamsToView, type TileOptions } from "@repo/lib";
+import _nodes from "../coordinates/nodes.json" assert { type: "json" };
+import tiles from "../coordinates/tiles.json" assert { type: "json" };
+import regions from "../coordinates/regions.json" assert { type: "json" };
+import _filters from "../coordinates/filters.json" assert { type: "json" };
+import _globalFilters from "../coordinates/global-filters.json" assert { type: "json" };
+
+const globalFilters = _globalFilters as GlobalFiltersCoordinates;
+const filters = _filters as FiltersCoordinates;
+const fIds = Object.values(filters).flatMap((f) => f.values.map((v) => v.id));
+const gIds = Object.values(globalFilters).flatMap((g) =>
+  g.values.map((v) => v.id),
+);
+
+const nodes = _nodes as NodesCoordinates;
+
+const InteractiveMapDynamic = dynamic(
+  () => import("@/components/interactive-map-dynamic"),
+  {
+    ssr: false,
+  },
+);
+
+export const metadata: Metadata = {
+  alternates: {
+    canonical: "/",
+  },
+  title: "Hogwarts Legacy Interactive Map – The Hidden Gaming Lair",
+  description:
+    "Explore Hogwarts Legacy with this Interactive Map! Showcasing all fast travel, sphinx puzzle spawn and other locations in Hogwarts, Hogsmeade and in the Overland!",
+};
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: Record<string, string | string[] | undefined>;
+}): JSX.Element {
+  const view = searchParamsToView(searchParams, fIds, gIds);
+
+  return (
+    <CoordinatesProvider
+      filters={filters}
+      mapName={Object.keys(tiles)[0]}
+      regions={regions as unknown as RegionsCoordinates}
+      globalFilters={globalFilters}
+      staticNodes={nodes}
+      view={view}
+    >
+      <HeaderOffset full>
+        <InteractiveMapDynamic />
+        <MarkersSearch tileOptions={tiles as unknown as TileOptions}>
+          <FloatingAds id="hogwarts" />
+        </MarkersSearch>
+      </HeaderOffset>
+    </CoordinatesProvider>
+  );
+}
--- services/data/src/hogwarts-legacy.ts
+++ services/data/src/hogwarts-legacy.ts
@@ -1,4 +1,4 @@
-import { CONTENT_DIR, initDirs } from "./lib/dirs.js";
+import { CONTENT_DIR, TEXTURE_DIR, initDirs } from "./lib/dirs.js";
 import { sqliteToJSON } from "./lib/db.js";
 import {
   readContentJSON,
@@ -7,12 +7,20 @@ import {
   writeJSON,
 } from "./lib/fs.js";
 import { decodeAvaf } from "./lib/avafdict.js";
+import { initNodes, writeNodes } from "./lib/nodes.js";
+import { initFilters, writeFilters } from "./lib/filters.js";
+import { generateTiles, initTiles, writeTiles } from "./lib/tiles.js";
+import { initTypesIDs, writeTypesIDs } from "./lib/types-ids.js";
 
 initDirs(
   "/mnt/c/dev/Hogwarts Legacy/Extracted/Data",
   "/mnt/c/dev/Hogwarts Legacy/Extracted/Texture",
   "/home/devleon/the-hidden-gaming-lair/static/hogwarts-legacy",
 );
+
+const nodes = initNodes();
+const filters = initFilters();
+const typesIDs = initTypesIDs();
 
 if (Bun.env.DB === "true") {
   readDirSync(CONTENT_DIR + "/Phoenix/Content/Localization/WIN64").forEach(
@@ -33,10 +41,24 @@ if (Bun.env.DB === "true") {
   sqliteToJSON("/Phoenix/Content/SQLiteDB");
 }
 
-const FastTravelLocations = readContentJSON(
+if (Bun.env.TILES === "true") {
+  const tiles = initTiles(
+    await generateTiles(
+      "overland",
+      TEXTURE_DIR +
+        "/Phoenix/Content/UI/HUD/MiniMap/MiniMapTiles/Overland/UI_T_MiniMap_Overland_8192_D.png",
+      44100,
+      512,
+    ),
+  );
+
+  writeTiles(tiles);
+}
+
+const FastTravelLocations = readContentJSON<FastTravelLocations>(
   "/Phoenix/Content/SQLiteDB/PhoenixGameData_FastTravelLocations.json",
 );
-const HogwartsMapIconTable = readContentJSON(
+const HogwartsMapIconTable = readContentJSON<HogwartsMapIconTable>(
   "/Phoenix/Content/SQLiteDB/PhoenixGameData_HogwartsMapIconTable.json",
 );
 const KnowledgeInvestigatable = readContentJSON(
@@ -57,3 +79,97 @@ const SphinxPuzzleLocations = readContentJSON(
 const enDict = readContentJSON(
   "/Phoenix/Content/Localization/WIN64/MAIN-enUS.json",
 );
+
+const hogwartsFastTravelLocations = FastTravelLocations.filter(
+  (location) => location.ShowOnMap,
+);
+
+for (const location of hogwartsFastTravelLocations) {
+  const mapIcon = HogwartsMapIconTable.find(
+    (icon) => icon.Name.toLowerCase() === location.Name.toLowerCase(),
+  );
+  const extension = mapIcon ? mapIcon.IconName.split("_").at(-1) : "Fireplaces";
+  let world;
+  if (location.Name.startsWith("FT_HW_")) {
+    world = "hogwarts";
+  } else if (location.Name.startsWith("FT_OL")) {
+    world = "overland";
+  } else if (location.Name.startsWith("FT_Hogsmeade")) {
+    world = "hogsmeade";
+  } else {
+    throw new Error(`Unknown world for ${location.Name}`);
+  }
+  const x =
+    mapIcon?.OverrideLocationX ||
+    location.BeaconLocationX ||
+    location.LocationX;
+  const y =
+    mapIcon?.OverrideLocationY ||
+    location.BeaconLocationY ||
+    location.LocationY;
+  const z =
+    mapIcon?.OverrideLocationZ ||
+    location.BeaconLocationZ ||
+    location.LocationZ;
+
+  handleLocation(location.Name, `fastTravel${extension}`, world, [x, y]);
+}
+
+writeNodes(nodes);
+writeFilters(filters);
+writeTypesIDs(typesIDs);
+
+function handleLocation(
+  id: string,
+  type: string,
+  mapName: string,
+  pos: [number, number],
+) {
+  let oldNodes = nodes.find((n) => n.type === type);
+  if (!oldNodes) {
+    oldNodes = { type: type, spawns: [] };
+    nodes.push(oldNodes);
+    oldNodes = nodes.find((n) => n.type === type)!;
+  }
+
+  if (
+    oldNodes.spawns.some(
+      (s) => s.p[0] === pos[0] && s.p[1] === pos[1] && s.mapName === mapName,
+    )
+  ) {
+    return;
+  }
+  oldNodes.spawns.push({
+    id,
+    p: pos,
+    mapName,
+  });
+}
+
+export type HogwartsMapIconTable = Array<{
+  Name: string;
+  OverrideLocationX: number;
+  OverrideLocationY: number;
+  OverrideLocationZ: number;
+  IconName: string;
+  Type: string;
+  Region: string;
+  ShowOnRegionTier: number;
+}>;
+
+type FastTravelLocations = Array<{
+  Name: string;
+  Available: any;
+  LocationX: number;
+  LocationY: number;
+  LocationZ: number;
+  WorldName: string;
+  IsSaveLocation: number;
+  IsFloo: number;
+  ZRot: number;
+  IsBuiltNightly: number;
+  ShowOnMap: number;
+  BeaconLocationX: number;
+  BeaconLocationY: number;
+  BeaconLocationZ: number;
+}>;
