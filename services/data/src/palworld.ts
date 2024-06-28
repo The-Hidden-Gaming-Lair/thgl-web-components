91995ffacb577ad1d4b48e45c298d331376e4fa0
lmachens <leon@machens.koeln>
2024-06-28 13:47:08 +0200

    Update some locations

--- services/data/src/palworld.ts
+++ services/data/src/palworld.ts
@@ -1,7 +1,8 @@
-import { CONTENT_DIR, TEXTURE_DIR, initDirs } from "./lib/dirs.js";
+import { CONTENT_DIR, OUTPUT_DIR, TEXTURE_DIR, initDirs } from "./lib/dirs.js";
 import { readJSON } from "./lib/fs.js";
 import { initNodes, writeNodes } from "./lib/nodes.js";
 import { generateTiles, initTiles, writeTiles } from "./lib/tiles.js";
+import { Node } from "./types.js";
 
 initDirs(
   "/mnt/c/dev/Palworld/Extracted/Data",
@@ -49,27 +50,62 @@ const mainWorld = readJSON<any>(
   CONTENT_DIR + "/Pal/Content/Pal/Maps/MainWorld_5/PL_MainWorld5.json",
 );
 
-const nodes = initNodes();
+const newNodes = initNodes();
 
+const oldNodes = readJSON<Node[]>(OUTPUT_DIR + "/coordinates/nodes.json");
 for (const node of mainWorld) {
-  if (node.Type !== "SceneComponent") {
+  if (node.Type !== "SceneComponent" || !node.Properties?.RelativeLocation) {
     continue;
   }
 
   let type: string;
+  let isStatic: boolean = false;
   if (node.Outer.startsWith("BP_DungeonPortalMarker_")) {
     type = "dungeon_random";
+  } else if (node.Outer.startsWith("BP_DungeonFixedEntrance")) {
+    type = "dungeon_sealed";
+  } else if (node.Outer.startsWith("BP_LevelObject_GoddessStatue")) {
+    type = "goddess_statue";
+    // } else if (node.Outer.startsWith("?")) {
+    //   type = "fasttravel";
+    // } else if (node.Outer.startsWith("?")) {
+    //   type = "boss_tower";
+  } else if (node.Outer.startsWith("BP_RelicObject")) {
+    type = "lifmunk_effigy";
+  } else if (node.Outer.startsWith("BP_PalMapObjectSpawner_SkillFruits")) {
+    type = "skill_fruit";
+  } else if (node.Outer.startsWith("BP_PalMapObjectSpawner_Treasure")) {
+    type = "treasure_box";
+    // } else if (node.Outer.startsWith("bp_palmapobjectspawner_palegg_grass_")) {
+    //   type = "egg_common";
+    // } else if (node.Outer.startsWith("bp_palmapobjectspawner_palegg_grass_")) {
+    //   type = "egg_dark";
   } else {
     continue;
   }
 
-  if (!nodes.some((n) => n.type === type)) {
-    nodes.push({
+  if (!newNodes.some((n) => n.type === type)) {
+    const nodeDef: Node = {
       type,
       spawns: [],
-    });
+    };
+    if (isStatic) {
+      nodeDef.static = true;
+    }
+    newNodes.push(nodeDef);
   }
-  const category = nodes.find((n) => n.type === type)!;
+  const category = newNodes.find((n) => n.type === type)!;
+  const spawn: Node["spawns"][0] = {
+    p: [node.Properties.RelativeLocation.X, node.Properties.RelativeLocation.Y],
+  };
+  category.spawns.push(spawn);
 }
 
+const nodes = oldNodes.map((n) => {
+  const newNodeType = newNodes.find((nn) => nn.type === n.type);
+  if (newNodeType) {
+    return newNodeType;
+  }
+  return n;
+});
 writeNodes(nodes);
