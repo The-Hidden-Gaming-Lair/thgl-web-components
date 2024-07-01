d2f0680bdf06fab9f0a84d811487b36d68cf7a4e
lmachens <leon@machens.koeln>
2024-07-01 11:08:26 +0200

    Update Nightingale

--- services/nightingale-data/coordinates.ts
+++ services/nightingale-data/coordinates.ts
@@ -1,62 +1,63 @@
 import { fileURLToPath } from "url";
 import typesIdMap from "../../static/nightingale/coordinates/types_id_map.json";
-import { writeJSON } from "./lib/fs.js";
+import { readJSON, writeJSON } from "./lib/fs.js";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 const OUT_DIR = __dirname + "out";
 const PROD_OUT_DIR = "/home/devleon/the-hidden-gaming-lair/static/nightingale";
 
-const response = await fetch(
-  "https://nightingale-api.th.gl/nodes?type=spawnNodes",
-);
-const data = (await response.json()) as Record<
-  string,
-  [number, number, number, string][]
->;
-
 const nodes: {
   type: string;
-  spawns: { p: [number, number]; mapName: string }[];
-}[] = [];
+  mapName: string;
+  spawns: { p: [number, number] }[];
+}[] = readJSON(PROD_OUT_DIR + "/coordinates/nodes.json");
 
-const maps = new Set<string>();
-Object.keys(typesIdMap).forEach((type) => {
-  const spawns = data[type];
-  if (spawns) {
-    const newSpawns: { p: [number, number]; mapName: string }[] = [];
-
-    spawns.forEach(([x, y, z, mapName]) => {
-      const isTooClose = newSpawns.some((s) => {
-        if (s.mapName !== mapName) {
-          return false;
+if (Bun.env.NODES === "true") {
+  const response = await fetch(
+    "https://nightingale-api.th.gl/nodes?type=spawnNodes",
+  );
+  const data = (await response.json()) as Record<
+    string,
+    [number, number, number, string][]
+  >;
+  const maps = new Set<string>();
+  Object.keys(typesIdMap).forEach((type) => {
+    const spawns = data[type];
+    if (spawns) {
+      spawns.forEach(([x, y, z, mapName]) => {
+        let oldNodes = nodes.find(
+          (n) => n.type === typesIdMap[type] && n.mapName === mapName,
+        );
+        if (!oldNodes) {
+          oldNodes = { type: typesIdMap[type], spawns: [], mapName };
+          nodes.push(oldNodes);
+          oldNodes = nodes.find(
+            (n) => n.type === typesIdMap[type] && n.mapName === mapName,
+          )!;
         }
-        const distance = calculateDistance(s, [x, y, z, mapName]);
-        const minDistance = getMinDistance(type);
-        return distance < minDistance;
-      });
-      if (isTooClose) {
-        return;
-      }
-      maps.add(mapName);
-      newSpawns.push({
-        p: [x, y],
-        mapName,
-      });
-    });
 
-    nodes.push({
-      type: typesIdMap[type],
-      spawns: newSpawns,
-    });
-  }
-});
-
-const types = Object.keys(data).sort();
+        const isTooClose = oldNodes.spawns.some((s) => {
+          const distance = calculateDistance(s, [x, y, z]);
+          const minDistance = getMinDistance(type);
+          return distance < minDistance;
+        });
+        if (isTooClose) {
+          return;
+        }
+        maps.add(mapName);
+        oldNodes!.spawns.push({
+          p: [x, y],
+        });
+      });
+    }
+  });
+  const types = Object.keys(data).sort();
+  writeJSON(OUT_DIR + "/creatures/types.json", types);
+  writeJSON(OUT_DIR + "/creatures/maps.json", Array.from(maps));
+}
 
 writeJSON(PROD_OUT_DIR + "/coordinates/nodes.json", nodes);
-writeJSON(OUT_DIR + "/creatures/types.json", types);
-writeJSON(OUT_DIR + "/creatures/maps.json", Array.from(maps));
 
 function getMinDistance(type: string) {
   if (type.startsWith("BP_Creature")) {
@@ -66,8 +67,8 @@ function getMinDistance(type: string) {
 }
 
 function calculateDistance(
-  node1: { p: [number, number]; mapName: string },
-  coords: [number, number, number, string],
+  node1: { p: [number, number] },
+  coords: [number, number, number],
 ) {
   const dx = node1.p[0] - coords[0];
   const dy = node1.p[1] - coords[1];
