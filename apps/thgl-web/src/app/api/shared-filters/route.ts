8894b76882ef01eb0bb9fb12a6729ba016de1e9b
lmachens <leon@machens.koeln>
2024-08-01 11:57:53 +0200

    Add shared filters

---
+++ apps/thgl-web/src/app/api/shared-filters/route.ts
@@ -0,0 +1,58 @@
+import { put, list } from "@vercel/blob";
+
+const headers = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, PUT, OPTIONS",
+  "Access-Control-Allow-Credentials": "true",
+  "Access-Control-Allow-Headers": "*",
+};
+export async function PUT(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const filename = searchParams.get("filename");
+  if (!filename) {
+    return Response.json(
+      { error: "No filename provided" },
+      { status: 400, headers },
+    );
+  }
+  if (!request.body) {
+    return Response.json(
+      { error: "No body provided" },
+      { status: 400, headers },
+    );
+  }
+  const existingName = filename.split("/shared-filters/")[1];
+  const name = existingName || filename;
+  const blob = await put(`/shared-filters/${name}`, request.body, {
+    access: "public",
+    contentType: "application/json",
+    cacheControlMaxAge: 0,
+    addRandomSuffix: !existingName,
+  });
+
+  return Response.json(blob, { headers });
+}
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const code = searchParams.get("code");
+  if (!code) {
+    return Response.json(
+      { error: "No code provided" },
+      { status: 400, headers },
+    );
+  }
+  const { blobs } = await list({ prefix: "shared-filters/" });
+  const blob = blobs.find((blob) => blob.url.endsWith(code));
+  if (!blob) {
+    return Response.json(
+      { error: "No shared filters found" },
+      { status: 404, headers },
+    );
+  }
+  return Response.json(blob, { headers });
+}
+
+export async function OPTIONS() {
+  return Response.json({}, { headers });
+}
