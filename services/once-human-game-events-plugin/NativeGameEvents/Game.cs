e29ac3f19e712b4b972efdabcea4c34e7a4dfd10
shalzuth <shalzuth@users.noreply.github.com>
2024-08-24 10:53:17 -0700

    OH - pos fix

--- services/once-human-game-events-plugin/NativeGameEvents/Game.cs
+++ services/once-human-game-events-plugin/NativeGameEvents/Game.cs
@@ -121,7 +121,8 @@ namespace NativeGameEvents
         internal static int modelMgrOffset = int.Parse("0xf88".Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
         internal static int locOffset = int.Parse("0x174".Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
         internal static int nameOffset = int.Parse("0x5d8".Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
-        internal static int playerOffset = int.Parse("0x1860".Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
+        //internal static int playerOffset = int.Parse("0x1848".Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
+        internal static int playerOffset = int.Parse("0xf38".Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
         [UnmanagedCallersOnly(EntryPoint = "GetActors")]
         internal static nint GetActors()
         {
