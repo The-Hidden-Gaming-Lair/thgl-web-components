538f3806ba668dadc358601abd53eca8c3c34fdf
lmachens <leon@machens.koeln>
2024-08-28 12:36:26 +0200

    Add enemy categories

--- services/data/src/black-myth-wukong.ts
+++ services/data/src/black-myth-wukong.ts
@@ -1,13 +1,21 @@
 import uniqolor from "uniqolor";
 import {
   CardDesc,
+  CommDropRuleDesc,
   FUStGuideGroupDesc,
   FUStSummonCommDesc,
+  FUStUnitBattleInfoExtendDesc,
   GMMapStaticData,
   ItemTreasureStart,
 } from "./black-myth-wukong.types.js";
 import { initDict, writeDict } from "./lib/dicts.js";
-import { CONTENT_DIR, initDirs, OUTPUT_DIR, TEMP_DIR } from "./lib/dirs.js";
+import {
+  CONTENT_DIR,
+  initDirs,
+  OUTPUT_DIR,
+  TEMP_DIR,
+  TEXTURE_DIR,
+} from "./lib/dirs.js";
 import {
   initFilters,
   initGlobalFilters,
@@ -20,7 +28,7 @@ import {
   readDirSync,
   readJSON,
 } from "./lib/fs.js";
-import { createBlankImage, saveIcon } from "./lib/image.js";
+import { createBlankImage, IconProps, saveIcon } from "./lib/image.js";
 import { initNodes, writeNodes } from "./lib/nodes.js";
 import { initRegions, writeRegions } from "./lib/regions.js";
 import { generateTiles, initTiles, writeTiles } from "./lib/tiles.js";
@@ -54,20 +62,45 @@ const fustGuideGroupDesc = await readJSON<FUStGuideGroupDesc>(
   CONTENT_DIR +
     "/b1/Content/00Main/PBTable/NoneRuntime/FUStGuideGroupDesc.json",
 );
+const commDropRuleDesc = await readJSON<CommDropRuleDesc>(
+  CONTENT_DIR + "/b1/Content/00Main/PBTable/Runtime/CommDropRuleDesc.json",
+);
 
 const cardDesc = await readJSON<CardDesc>(
   CONTENT_DIR + "/b1/Content/00Main/PBTable/Runtime/CardDesc.json",
 );
+const fUStUnitBattleInfoExtendDesc: FUStUnitBattleInfoExtendDesc = { "1": [] };
+const fUStUnitBattleInfoExtendDescs = readDirSync(
+  CONTENT_DIR + "/b1/Content/00Main/PBTable/NoneRuntime",
+).filter(
+  (f) => f.startsWith("FUStUnitBattleInfoExtendDesc") && f.endsWith(".json"),
+);
+for (const file of fUStUnitBattleInfoExtendDescs) {
+  const part = await readJSON<FUStUnitBattleInfoExtendDesc>(
+    CONTENT_DIR + "/b1/Content/00Main/PBTable/NoneRuntime/" + file,
+  );
+  if (part["1"]) {
+    fUStUnitBattleInfoExtendDesc["1"].push(...part["1"]);
+  }
+}
 
 const gmMapStaticData = await readJSON<GMMapStaticData>(
   CONTENT_DIR +
     "/b1/Content/00Main/PBTable/GMMapStaticData/GMMapStaticData.json",
 );
 
+const iconNames = readDirSync(
+  TEXTURE_DIR + "/b1/Content/00MainHZ/UI/AlwaysCook/Icon/",
+);
+
 const typeIdsToResId = Object.fromEntries(
   Object.values(gmMapStaticData.MapData).flatMap((map) => {
     return Object.values(map).map((v) => {
-      const last = v.Guid.split("-").at(-1)!;
+      const [, , ...more] = v.Guid.split("-").at(-1)!.split("_");
+      if (more.length === 0) {
+        return [];
+      }
+      const last = more.join("_");
       let typeId;
       if (last.includes("_C")) {
         typeId = last.split("_C")[0];
@@ -78,7 +111,194 @@ const typeIdsToResId = Object.fromEntries(
     });
   }),
 );
-const mapNames = ["HFS01", "HFM02"];
+const typeIdsToExtendId = Object.fromEntries(
+  Object.values(gmMapStaticData.MapData).flatMap((map) => {
+    return Object.values(map).map((v) => {
+      const [, , ...more] = v.Guid.split("-").at(-1)!.split("_");
+      if (more.length === 0) {
+        return [];
+      }
+      const last = more.join("_");
+      let typeId;
+      if (last.includes("_C")) {
+        typeId = last.split("_C")[0];
+      } else {
+        typeId = last.replace(/_[^_]*$/, "");
+      }
+      return [typeId.toLowerCase(), v.ExtendId];
+    });
+  }),
+);
+const PORTRAITS: Record<string, string> = {
+  "1": "lesser_yaoguais",
+  "2": "yaoguai_chiefs",
+  "3": "yaoguai_kings",
+  "4": "characters",
+};
+enDict["lesser_yaoguais"] = "Lesser Yaoguais";
+enDict["yaoguai_chiefs"] = "Yaoguai Chiefs";
+enDict["yaoguai_kings"] = "Yaoguai Kings";
+enDict["characters"] = "Characters";
+
+// Unit_LYS_YaXiangKe_1a  = Crow Diviner
+for (const folder of readDirSync(
+  CONTENT_DIR + "/b1/Content/00Main/Design/Units/",
+)) {
+  if (folder.endsWith(".json") || folder === "Player") {
+    continue;
+  }
+  for (const file of readDirSync(
+    CONTENT_DIR + `/b1/Content/00Main/Design/Units/${folder}/`,
+  )) {
+    if (
+      !file.startsWith("Unit") ||
+      file.includes("emptyunit") ||
+      file.includes("test")
+    ) {
+      continue;
+    }
+    // const unit = await readJSON<UEObjects>(CONTENT_DIR + `/b1/Content/00Main/Design/Units/${folder}/` + file);
+    const typeId = (file.replace(".json", "") + "_C").toLowerCase();
+    const [, , ...more] = typeId.split("_");
+    let type = more.join("_");
+    if (type.includes("_c")) {
+      type = type.split("_c")[0];
+    } else {
+      type = type.replace(/_[^_]*$/, "");
+    }
+    if (typesIDs[typeId]) {
+      throw new Error(`Type already exists: ${typeId}`);
+    }
+    typesIDs[typeId] = type;
+
+    let iconPath;
+    const iconProps: IconProps = {};
+    let group = "enemies";
+    const typeAlt = type.replace(/_[^_]*$/, "");
+    const resId = typeIdsToResId[type] || typeIdsToResId[typeAlt];
+    const extendId = typeIdsToExtendId[type] || typeIdsToExtendId[typeAlt];
+    if (!resId || !extendId) {
+      console.warn(`resId not found for ${type} | ${typeAlt} | ${file}`);
+    } else {
+      const guideGroupDesc = fustGuideGroupDesc["1"].find(
+        (f) => f["1"] === resId,
+      );
+      const fUStUnitBattleInfo = fUStUnitBattleInfoExtendDesc["1"].find(
+        (f) => f["1"] === extendId,
+      );
+
+      if (
+        fUStUnitBattleInfo &&
+        en[`StringKVMapDesc.UnitBattleInfoExtendDesc.${extendId}.UnitName`]
+      ) {
+        const card = cardDesc["1"].find(
+          (c) => c["2"] === fUStUnitBattleInfo["2"],
+        );
+        if (card) {
+          const portraitId = card["4"]!;
+          group = PORTRAITS[portraitId] || "enemies";
+        }
+        enDict[type] =
+          en[`StringKVMapDesc.UnitBattleInfoExtendDesc.${extendId}.UnitName`];
+      } else if (!guideGroupDesc) {
+        console.warn(`guideGroupDesc not found for ${type} | ${typeAlt}`);
+      } else {
+        const cardGroup = guideGroupDesc["3"]!;
+        const card = cardDesc["1"].find((c) => c["10"]?.includes(cardGroup));
+        if (!card) {
+          console.warn(
+            `card not found for ${type} | ${typeAlt} | ${resId} ${cardGroup}`,
+          );
+        } else {
+          const cardId = card["1"];
+          const portraitId = card["4"]!;
+          group = PORTRAITS[portraitId] || "enemies";
+
+          enDict[type] = en[`StringKVMapDesc.CardDesc.${cardId}.UnitName`];
+          enDict[`${type}_desc`] =
+            en[`StringKVMapDesc.CardDesc.${cardId}.UnitPoetry`];
+        }
+      }
+
+      const commDropRule = commDropRuleDesc["1"].find((c) => c["1"] === resId);
+      if (commDropRule) {
+        if ("13" in commDropRule) {
+          const itemIds = commDropRule["13"];
+          if (itemIds) {
+            enDict[`${type}_desc`] = `<b>Drops:<b><br>`;
+            if (Array.isArray(itemIds)) {
+              for (const itemId of itemIds) {
+                if (en[`StringKVMapDesc.ItemDesc.${itemId}.Name`]) {
+                  enDict[`${type}_desc`] +=
+                    en[`StringKVMapDesc.ItemDesc.${itemId}.Name`] + "<br>";
+                }
+              }
+            } else {
+              const itemId = itemIds["1"];
+              if (en[`StringKVMapDesc.ItemDesc.${itemId}.Name`]) {
+                enDict[`${type}_desc`] +=
+                  en[`StringKVMapDesc.ItemDesc.${itemId}.Name`];
+              }
+            }
+          }
+        }
+      }
+    }
+    if (!enDict[type]) {
+      enDict[type] = type;
+    }
+
+    let category = filters.find((f) => f.group === group);
+    if (!category) {
+      filters.push({
+        group: group,
+        defaultOpen: true,
+        defaultOn: true,
+        values: [],
+      });
+      category = filters.find((f) => f.group === group)!;
+    }
+    if (!category.values.some((v) => v.id === type)) {
+      if (iconPath) {
+        //
+      } else if (enDict[type].startsWith("Crow ")) {
+        iconPath =
+          "/home/devleon/the-hidden-gaming-lair/static/global/icons/game-icons/raven_lorc.webp";
+        iconProps.color = uniqolor(type, {
+          lightness: [70, 80],
+        }).color;
+      } else if (enDict[type].startsWith("Wolf ")) {
+        iconPath =
+          "/home/devleon/the-hidden-gaming-lair/static/global/icons/game-icons/wolf-head_lorc.webp";
+        iconProps.color = uniqolor(type, {
+          lightness: [70, 80],
+        }).color;
+      } else if (enDict[type].startsWith("Croaky")) {
+        iconPath =
+          "/home/devleon/the-hidden-gaming-lair/static/global/icons/game-icons/frog_lorc.webp";
+        iconProps.color = uniqolor(type, {
+          lightness: [70, 80],
+        }).color;
+      } else {
+        iconPath =
+          "/home/devleon/the-hidden-gaming-lair/static/global/icons/game-icons/targeted_sbed.webp";
+        iconProps.color = uniqolor(type, {
+          lightness: [70, 80],
+        }).color;
+      }
+      const iconName = await saveIcon(iconPath, type, iconProps);
+      const size = 1;
+      category.values.push({
+        id: type,
+        icon: iconName,
+        size,
+      });
+    }
+  }
+}
+
+// const mapNames = ["HFS01", "HFM02"];
+const mapNames = ["HFS01"];
 enDict["HFS01"] = "Chapter 1";
 enDict["HFM02"] = "Chapter 2";
 const mapFolders = readDirSync(CONTENT_DIR + "/b1/Content/00Main/Maps");
@@ -109,9 +329,9 @@ for (const mapName of mapFolders) {
   );
   let i = 1;
   for (const file of files) {
-    console.log(
-      `File: ${i++}/${files.length}: ${file.replace(`${CONTENT_DIR}/b1/Content/00Main/Maps/${mapName}/`, "")}`,
-    );
+    // console.log(
+    //   `File: ${i++}/${files.length}: ${file.replace(`${CONTENT_DIR}/b1/Content/00Main/Maps/${mapName}/`, "")}`,
+    // );
 
     if (!file.endsWith(".json") || file.includes("Audio")) {
       continue;
@@ -145,7 +365,7 @@ for (const mapName of mapFolders) {
       let type;
       let iconName;
       let size = 2;
-      let typeId = item.Outer.split("_C")[0] + "_C";
+      let typeId = (item.Outer.split("_C")[0] + "_C").toLowerCase();
       if (item.Outer.startsWith("BP_yaocai_lingzhi_C")) {
         group = "materials";
         enDict[group] = "Materials";
@@ -172,6 +392,47 @@ for (const mapName of mapFolders) {
             threshold: 0,
           },
         );
+      } else if (item.Outer.startsWith("BP_yaocai_longdancao_C")) {
+        group = "materials";
+        enDict[group] = "Materials";
+        type = "gentian";
+        enDict[type] = en["StringKVMapDesc.ItemDesc.3206.Name"];
+        iconName = await saveIcon(
+          "/b1/Content/00MainHZ/UI/AlwaysCook/Icon/Item_Icon_3206_t.png",
+          type,
+          {
+            // color: "#ffffff",
+            threshold: 0,
+          },
+        );
+      } else if (item.Outer.startsWith("BP_yaocai_biou_")) {
+        group = "materials";
+        enDict[group] = "Materials";
+        type = "jade_lotus";
+        enDict[type] = en["StringKVMapDesc.ItemDesc.3216.Name"];
+        iconName = await saveIcon(
+          "/b1/Content/00MainHZ/UI/AlwaysCook/Icon/Item_Icon_3216_t.png",
+          type,
+          {
+            // color: "#ffffff",
+            threshold: 0,
+          },
+        );
+        size = 1.5;
+      } else if (item.Outer.startsWith("BP_GhostFireSpawner_")) {
+        group = "items";
+        enDict[group] = "Items";
+        type = "will";
+        enDict[type] = en["StringKVMapDesc.ItemDesc.1002.Name"];
+        iconName = await saveIcon(
+          "/b1/Content/00MainHZ/UI/AlwaysCook/Icon/Item_Icon_1002_g.png",
+          type,
+          {
+            // color: "#ffffff",
+            threshold: 0,
+          },
+        );
+        size = 1.5;
       } else if (item.Outer.startsWith("RebirthPoint")) {
         group = "locations";
         enDict[group] = "Locations";
@@ -190,69 +451,17 @@ for (const mapName of mapFolders) {
       } else if (item.Outer.startsWith("TAMER_")) {
         group = "enemies";
         enDict[group] = "Enemies";
-        type = item.Outer.split("_C")[0]
-          .replace("BP_", "")
-          .replace("TAMER_", "")
-          .replace("_huoba", "");
-
-        const typeId1 = typeId.replace("_C", "").toLowerCase();
-        const typeId2 = typeId1.replace("tamer_", "unit_");
-        const resId = typeIdsToResId[typeId1] || typeIdsToResId[typeId2];
-        if (!resId) {
-          console.warn(`resId not found for ${typeId1} | ${typeId2}`);
+        typeId = typeId.replace("tamer_", "unit_").replace("_huoba", "");
+        type = typesIDs[typeId];
+        if (!type) {
+          const [, , searchId] = typeId.split("_");
+          type = Object.entries(typesIDs).find(([k, v]) => {
+            return k.includes(searchId);
+          })?.[1];
+        }
+        if (!type) {
+          console.warn(`Type not found for ${typeId}`);
           continue;
-        }
-
-        const guideGroupDesc = fustGuideGroupDesc["1"].find(
-          (f) => f["1"] === resId,
-        );
-        if (!guideGroupDesc) {
-          // console.warn(`GuideGroupDesc not found for ${typeId}`);
-          enDict[type] = type;
-        } else {
-          const cardGroup = guideGroupDesc["3"]!;
-          const card = cardDesc["1"].find((c) =>
-            c["10"]?.startsWith(cardGroup),
-          );
-          if (!card) {
-            // console.warn(`Card not found for ${typeId}: ${cardGroup}`);
-            enDict[type] = type;
-          } else {
-            const cardId = card["1"];
-
-            enDict[type] = en[`StringKVMapDesc.CardDesc.${cardId}.UnitName`];
-            enDict[`${type}_desc`] =
-              en[`StringKVMapDesc.CardDesc.${cardId}.UnitPoetry`];
-          }
-        }
-
-        if (enDict[type].includes("Wolf")) {
-          iconName = await saveIcon(
-            "/home/devleon/the-hidden-gaming-lair/static/global/icons/game-icons/wolf-head_lorc.webp",
-            type,
-            {
-              color: uniqolor(type, {
-                lightness: [70, 80],
-              }).color,
-            },
-          );
-        } else {
-          iconName = await saveIcon(
-            "/home/devleon/the-hidden-gaming-lair/static/global/icons/game-icons/targeted_sbed.webp",
-            type,
-            {
-              color: uniqolor(type, {
-                lightness: [70, 80],
-              }).color,
-            },
-          );
-        }
-        size = 1;
-
-        if (typeId.startsWith("TAMER_")) {
-          typesIDs[typeId.replace("TAMER_", "UNIT_")] = type;
-        } else {
-          typesIDs[typeId] = type;
         }
       } else if (item.Outer.startsWith("BPO_TreasureBox_")) {
         group = "items";
@@ -287,7 +496,7 @@ for (const mapName of mapFolders) {
         // );
         // size = 0.8;
       }
-
+      typesIDs[typeId] = type;
       let category = filters.find((f) => f.group === group);
 
       if (!category) {
@@ -299,7 +508,7 @@ for (const mapName of mapFolders) {
         });
         category = filters.find((f) => f.group === group)!;
       }
-      if (!category.values.some((v) => v.id === type)) {
+      if (!category.values.some((v) => v.id === type) && iconName) {
         category.values.push({
           id: type,
           icon: iconName,
@@ -384,7 +593,45 @@ Object.keys(tiles).forEach((mapName) => {
   );
 });
 
-writeFilters(filters);
+const sortPriority = [
+  "locations",
+  "items",
+  "materials",
+  "lesser_yaoguais",
+  "yaoguai_chiefs",
+  "yaoguai_kings",
+  "characters",
+  "enemies",
+];
+const sortedFilters = filters
+  // .map((f) => {
+  //   return {
+  //     ...f,
+  //     values: f.values.filter((v) => nodes.some((n) => n.type === v.id)),
+  //   };
+  // })
+  .sort((a, b) => {
+    if (a.group === b.group) {
+      return 0;
+    }
+    let priorityA = sortPriority.findIndex((p) =>
+      a.group.toLowerCase().startsWith(p.toLowerCase()),
+    );
+    if (priorityA === -1) {
+      priorityA = 1000;
+    }
+    let priorityB = sortPriority.findIndex((p) =>
+      b.group.toLowerCase().startsWith(p.toLowerCase()),
+    );
+    if (priorityB === -1) {
+      priorityB = 1000;
+    }
+    if (priorityA === priorityB) {
+      return a.group.localeCompare(b.group);
+    }
+    return priorityA - priorityB;
+  });
+writeFilters(sortedFilters);
 writeTypesIDs(typesIDs);
 writeDict(enDict, "en");
 writeTiles(tiles);
