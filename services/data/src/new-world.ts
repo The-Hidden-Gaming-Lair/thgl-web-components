7940cda51b34d16ee194be1a6f8db37846d5da26
lmachens <leon@machens.koeln>
2024-09-13 15:48:26 +0200

    Generate NW tiles

--- services/data/src/new-world.ts
+++ services/data/src/new-world.ts
@@ -1,7 +1,21 @@
-import { initDirs } from "./lib/dirs.js";
+import { $ } from "bun";
+import { readDirRecursive } from "./lib/fs.js";
 
-initDirs(
-  "/mnt/c/dev/New World/pre_ob_datatables",
-  "/mnt/c/dev/New World/worldtiles",
-  "/home/devleon/aeternum-map/apps/api/public",
-);
+const DATA_PATH = "/mnt/c/dev/New World/Data";
+const TEXTURE_DIR = "/mnt/c/dev/New World/Textures";
+const OUTPUT_DIR = "/home/devleon/aeternum-map/apps/api/public/ptr";
+
+// await $`/mnt/c/dev/New\ World/texconv.exe -y -r:keep 'C:\dev\New World\Textures\worldtiles\*.dds' -ft png -o 'C:\dev\New World\Textures\worldtiles'`;
+console.log("Converted DDS to PNG");
+for (const file of readDirRecursive(TEXTURE_DIR + "/worldtiles")) {
+  try {
+    if (file.endsWith(".png")) {
+      const outFile = file
+        .replace(TEXTURE_DIR + "/worldtiles", OUTPUT_DIR)
+        .replace(".png", ".webp");
+      await $`cwebp "${file}" -m 6 -o "${outFile}" -quiet`;
+    }
+  } catch (e) {
+    console.error(`Failed to convert ${file}`);
+  }
+}
