9ef006060d110dd7e3338d5b8be48bac8b0e77ac
lmachens <leon@machens.koeln>
2024-09-16 09:28:12 +0200

    Delay plugin loading

--- apps/once-human-overwolf/src/background.tsx
+++ apps/once-human-overwolf/src/background.tsx
@@ -10,27 +10,31 @@ initBackground(
   "1271431538675814461",
 );
 
-const gameEventsPlugin = await loadPlugin<GameEventsPlugin>("game-events");
+async function runPlugin() {
+  const gameEventsPlugin = await loadPlugin<GameEventsPlugin>("game-events");
 
-const refreshProcess = () => {
-  gameEventsPlugin.UpdateProcess(handleCallback, handleError, "ONCE_HUMAN");
-};
+  const refreshProcess = () => {
+    gameEventsPlugin.UpdateProcess(handleCallback, handleError, "ONCE_HUMAN");
+  };
 
-let status = "";
-const handleCallback = () => {
-  if (status !== "ok") {
-    status = "ok";
-    console.log("Game Events Plugin is running");
-  }
-  setTimeout(refreshProcess, 1000);
-};
+  let status = "";
+  const handleCallback = () => {
+    if (status !== "ok") {
+      status = "ok";
+      console.log("Game Events Plugin is running");
+    }
+    setTimeout(refreshProcess, 1000);
+  };
 
-const handleError = (err: string) => {
-  if (err !== status) {
-    status = err;
-    console.error("Game Events Plugin Error: ", err);
-  }
-  setTimeout(refreshProcess, 1000);
-};
+  const handleError = (err: string) => {
+    if (err !== status) {
+      status = err;
+      console.error("Game Events Plugin Error: ", err);
+    }
+    setTimeout(refreshProcess, 1000);
+  };
 
-refreshProcess();
+  refreshProcess();
+}
+
+setTimeout(runPlugin, 1000);
