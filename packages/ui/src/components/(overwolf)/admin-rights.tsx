55a15a495f39f907d32cf49c01984d078df924f0
lmachens <leon@machens.koeln>
2024-09-23 09:08:31 +0200

    Add "Exit Overwolf" image

--- packages/ui/src/components/(overwolf)/admin-rights.tsx
+++ packages/ui/src/components/(overwolf)/admin-rights.tsx
@@ -9,6 +9,7 @@ import {
 import { useGameState } from "@repo/lib";
 import { Button } from "../(controls)";
 import { closeMainWindow } from "@repo/lib/overwolf";
+import { ScrollArea } from "../ui/scroll-area";
 
 export function AdminRights() {
   const error = useGameState((state) => state.error) ?? "";
@@ -23,51 +24,59 @@ export function AdminRights() {
         <AlertDialogHeader>
           <AlertDialogTitle>⚠️ Missing Permissions</AlertDialogTitle>
         </AlertDialogHeader>
-        <p>
-          This app requires administrator rights and needs to be running on game
-          start to function properly. Exit Overwolf by right-clicking on task
-          bar icon, then run it as an administrator{" "}
-          <span className="font-bold">before starting the game</span>.
-        </p>
-        <ul className="mb-4 list-disc list-inside">
-          <li>
-            <span className="font-bold">Exit Overwolf:</span> Right-click on the
-            system tray icon and select{" "}
-            <span className="italic">"Exit Overwolf"</span>.
-          </li>
-          <li>
-            <span className="font-bold">Right-click Method:</span> Locate the
-            Overwolf shortcut or executable file, right-click on it, and select{" "}
-            <span className="italic">"Run as administrator"</span>.
-          </li>
-          <li>
-            <span className="font-bold">Properties Method:</span> Right-click
-            the Overwolf shortcut, select{" "}
-            <span className="italic">"Properties"</span>, go to the{" "}
-            <span className="italic">"Compatibility"</span> tab, and check the
-            box labeled{" "}
-            <span className="italic">
-              "Run this program as an administrator"
-            </span>
-            . Click <span className="italic">"Apply"</span> and then{" "}
-            <span className="italic">"OK"</span>.
-          </li>
-          <li>
-            <span className="font-bold">Need Help?</span> If you have any
-            questions or need assistance, feel free to join my Discord server
-            and check the FAQ and support channels:
-            <ExternalAnchor
-              href="https://www.th.gl/discord"
-              className="flex gap-1 text-primary hover:underline"
-            >
-              <span>Join my Discord server</span>
-              <ExternalLink className="w-3 h-3" />
-            </ExternalAnchor>
-          </li>
-        </ul>
-        <p className="text-secondary-foreground">
-          Thank you for your understanding and support! 🙏
-        </p>
+        <ScrollArea className="max-h-[70vh]">
+          <p>
+            This app requires administrator rights and needs to be running on
+            game start to function properly. Exit Overwolf, then run it as an
+            administrator{" "}
+            <span className="font-bold">before starting the game</span>.
+          </p>
+          <ul className="mb-4 list-disc list-inside">
+            <li>
+              <span className="font-bold">Exit Overwolf:</span> Right-click on
+              the system tray icon and select{" "}
+              <span className="italic">"Exit Overwolf"</span>.
+              <img
+                src="/global_icons/guide/exit.webp"
+                alt="Exit Overwolf"
+                height={96}
+                width={270}
+              />
+            </li>
+            <li>
+              <span className="font-bold">Properties Method:</span> Right-click
+              the Overwolf shortcut, select{" "}
+              <span className="italic">"Properties"</span>, go to the{" "}
+              <span className="italic">"Compatibility"</span> tab, and check the
+              box labeled{" "}
+              <span className="italic">
+                "Run this program as an administrator"
+              </span>
+              . Click <span className="italic">"Apply"</span> and then{" "}
+              <span className="italic">"OK"</span>.
+            </li>
+            <li>
+              <span className="font-bold">Right-click Method:</span> Locate the
+              Overwolf shortcut or executable file, right-click on it, and
+              select <span className="italic">"Run as administrator"</span>.
+            </li>
+            <li>
+              <span className="font-bold">Need Help?</span> If you have any
+              questions or need assistance, feel free to join my Discord server
+              and check the FAQ and support channels:
+              <ExternalAnchor
+                href="https://www.th.gl/discord"
+                className="flex gap-1 text-primary hover:underline"
+              >
+                <span>Join my Discord server</span>
+                <ExternalLink className="w-3 h-3" />
+              </ExternalAnchor>
+            </li>
+          </ul>
+          <p className="text-secondary-foreground">
+            Thank you for your understanding and support! 🙏
+          </p>
+        </ScrollArea>
         <Button onClick={() => closeMainWindow()}>Close App</Button>
       </AlertDialogContent>
     </AlertDialog>
