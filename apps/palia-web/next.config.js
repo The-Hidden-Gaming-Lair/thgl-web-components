e4fd49f4b405243c9e61c91ecd44993ccc804c13
lmachens <leon@machens.koeln>
2024-10-07 15:50:46 +0200

    Add redirects

--- apps/palia-web/next.config.js
+++ apps/palia-web/next.config.js
@@ -13,6 +13,45 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/:lang/rummage-pile",
+        destination: "/rummage-pile",
+        permanent: true,
+      },
+      {
+        source: "/:lang/leaderboard",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/:lang/Kilima%20Valley",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/:lang/Bahari%20Bay",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/:lang/Fairgrounds",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/:lang/Housing",
+        destination: "/",
+        permanent: true,
+      },
+      {
+        source: "/:lang/download",
+        destination: "https://www.overwolf.com/app/Leon_Machens-Palia_Map",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
